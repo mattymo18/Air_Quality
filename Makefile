ce1437e1cf14499c67546f80b311395a8f3abd2f
MATT JOHNSON <johnson.matt1818@gmail.com>
2020-10-20 13:23:41 -0400

    Added cleaned greenville data

--- Makefile
+++ Makefile
@@ -4,14 +4,17 @@ clean:
 
 	rm derived_data/*.csv
 			
-		
+derived_data/Greenville.Clean.csv\
 derived_data/Raleigh.Clean.csv:\
  Source_Data/Raleigh_Air.csv\
+ Source_Data/Greenville_Air.csv\
  tidy_data.R
 	Rscript tidy_data.R
 	
+derived_data/Greenville.Clean.Covid.csv\
 Raleigh.Clean.Covid.csv:\
  derived_data/Raleigh.Clean.csv\
+ derived_data/Greenville.Clean.csv\
  Source_Data/covid_confirmed_usafacts.csv\
  tidy_data.R
 	Rscript tidy_data.R
--- tidy_data.R
+++ tidy_data.R
@@ -2,6 +2,8 @@ library(tidyverse)
 
 Raleigh_Air <- read.csv("Source_Data/Raleigh_Air.csv") %>% 
   select(city, utc, parameter, value, latitude, longitude)
+Greenville_Air <- read.csv("Source_Data/Greenville_Air.csv") %>% 
+  select(city, utc, parameter, value, latitude, longitude)
 Raleigh <- na.omit(spread(Raleigh_Air, key = parameter, value = value))
 
 Raleigh$utc = as.Date(Raleigh$utc) 
@@ -20,7 +22,25 @@ Raleigh.Clean <- Raleigh %>%
   arrange(utc) %>% 
   select(Location, utc, CO, NO2, O3, PM10, PM25, SO2)
 
+Greenville <- spread(Greenville_Air, key = parameter, value = value)
+
+Greenville$utc = as.Date(Greenville$utc) 
+Greenville$Time <- format(Greenville$utc,"%H:%M:%S")
+Greenville = Greenville %>% 
+  select(-Time)
+Greenville.Clean <- Greenville %>% 
+  group_by(utc, .drop = T) %>% 
+  summarise(PM25 = mean(pm25)) %>% 
+  mutate(Location = "Greenville") %>% 
+  arrange(utc) %>% 
+  select(Location, utc, PM25)
+
 write.csv(Raleigh.Clean, "derived_data/Raleigh.Clean.csv")
+write.csv(Greenville.Clean, "derived_data/Greenville.Clean.csv")
+
+
+
+
 
 library(lubridate)
 
@@ -48,3 +68,23 @@ write.csv(Raleigh_Clean_COVID, "derived_data/Raleigh.Clean.Covid.csv")
 
 
 
+Greenville_Clean <- read_csv("derived_data/Greenville.Clean.csv")
+
+#Begin_Date = gsub("^0", "", format(min(Greenville_Clean$utc), format = "%m/%d/%Y"))
+#End_Date = gsub("^0", "", format(max(Greenville_Clean$utc), format = "%m/%d/%Y"))
+Confirmed_Cases_Greenville <- subset(Confirmed_Cases, State =="NC" & countyFIPS == 37147, select = `1/22/20`:`10/18/20`)
+
+day <- seq(10,21)
+for (i in day) {
+  Confirmed_Cases_Greenville[[paste("1/", i,"/20",sep = "")]] = 0
+}
+
+Pivoted_Cases <- Confirmed_Cases_Greenville %>%
+  pivot_longer(cols = `1/22/20`:`1/21/20`,names_to = "utc", values_to = "Cases")
+
+Pivoted_Cases$utc <- as.Date(Pivoted_Cases$utc, "%m/%d/%Y")
+year(Pivoted_Cases$utc) <- 2020
+
+Greenville_Clean_COVID <- merge(Greenville_Clean, Pivoted_Cases,by="utc")
+
+write.csv(Greenville_Clean_COVID, "derived_data/Greenville.Clean.Covid.csv")
